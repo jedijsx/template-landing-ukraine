1138831b7d2c1a1fb1a6aac5d825167d1ba8afdc
Dmytro Puchkov <puchkov.dev@gmail.com>
2023-07-20 19:51:48 +0300

    Update gulp, Update gitignore, Create new section

--- gulpfile.js
+++ gulpfile.js
@@ -13,4 +13,12 @@ gulp.task('browser-sync', function() {
   gulp.watch('src/js/*.js').on('change', browserSync.reload);
 });
 
+gulp.task('images', async function(){
+  const imagemin = await import('gulp-imagemin');
+
+  return gulp.src('src/images/original/*')
+      .pipe(imagemin.default())
+      .pipe(gulp.dest('src/images'));
+});
+
 gulp.task('default', gulp.series('browser-sync'));
